f38fd99184e0f1ffca948b4700c87878c33ad41d
hsm-hx <skytea922@gmail.com>
2018-08-25 12:29:17 +0900

    :rocket: Debug to Heroku

--- heroku_exec.sh
+++ heroku_exec.sh
@@ -3,8 +3,10 @@
   -u user.dic \
   -f utf-8 -t utf-8 user.csv
 
-mv original.dic /app/vendor/mecab/lib/mecab/dic/ipadic/
+mv user.dic /app/vendor/mecab/lib/mecab/dic/ipadic/
 
 echo "userdic = /app/vendor/mecab/dic/ipadic/user.dic" >> /app/vendor/mecab/lib/mecab/dic/ipadic/dicrc
+
+find /app/vendor/ -name "*mecab*"
 
 ruby bot.rb
