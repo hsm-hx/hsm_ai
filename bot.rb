0033a37981ef4c392b6f3168e4e973d75b957290
hsm-hx <skytea922@gmail.com>
2018-08-25 14:24:56 +0900

    :bug: RTとブログ投稿を除外

--- bot.rb
+++ bot.rb
@@ -40,7 +40,7 @@ class Bot
     
     @client.user_timeline(user_name, {count: tweet_count, exclude: retweets}).each do |timeline|
       tweet = @client.status(timeline.id)
-      if not (tweet.text.include?("RT") and tweet.text.include?("hatenablog"))
+      if not (tweet.text.include?("RT") or tweet.text.include?("hatenablog"))
         tweets.push(tweet2textdata(tweet.text))
       end
     end
